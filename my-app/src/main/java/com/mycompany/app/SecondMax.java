dd1f65064bb92ed70623827e13aaf753655c0411
Ann <anna.poddubnaya@gmail.com>
2015-07-08 17:36:21 +0300

    guava dependency added

--- my-app/src/main/java/com/mycompany/app/SecondMax.java
+++ my-app/src/main/java/com/mycompany/app/SecondMax.java
@@ -1,51 +1,50 @@
 package com.mycompany.app;
 
+import com.google.common.base.Preconditions;
 
-public class SecondMax
-{
+public class SecondMax {
 
-    public static int getSecondMax(int[] numbers){
-       
-        if(numbers==null){
-            throw new IllegalArgumentException("you should input some array");
-        }
-        if(numbers.length==0){
-            throw new IllegalArgumentException("array is empty");
-        }
-        if(numbers.length==1){
-        	throw new IllegalArgumentException("array is too small");
-        }
-        
-        int max;
-        int secondMax;
-        max = numbers[0];
-        int i=0;
-        while(i<numbers.length && numbers[0]==numbers[i]){
-        	i++;
-        }
-        if(i==numbers.length){
-        	throw new IllegalArgumentException("all elements are equal");
-        }
-        
-        if(numbers[0]>numbers[i]){
-        	secondMax = numbers[i];
-        }
-        else{
-        	max = numbers[i];
-        	secondMax = numbers[0];
-        }
-        i++;
-        while(i<numbers.length){
-        	if(numbers[i]>max){
-        		secondMax = max;
-        		max = numbers[i];
-        	}
-        	else if(numbers[i]<max && numbers[i]>secondMax){
-        		secondMax = numbers[i];
-        	}
-        	i++;
-        }
-        
-        return secondMax;
-    }
+	public static int getSecondMax(int[] numbers) {
+		Preconditions.checkNotNull(numbers, "array is empty");
+		Preconditions.checkArgument(numbers.length != 0, "array is empty");
+		Preconditions.checkArgument(numbers.length != 1, "array is too small");
+		Preconditions.checkArgument(hasDifferentValues(numbers),
+				"all elements are equal");
+
+		int max = numbers[0];
+		int secondMax;
+
+		int i = 0;
+		while (numbers[0] == numbers[i]) {
+			i++;
+		}
+		if (numbers[0] > numbers[i]) {
+			secondMax = numbers[i];
+		} else {
+			max = numbers[i];
+			secondMax = numbers[0];
+		}
+		i++;
+
+		while (i < numbers.length) {
+			if (numbers[i] > max) {
+				secondMax = max;
+				max = numbers[i];
+			} else if (numbers[i] < max && numbers[i] > secondMax) {
+				secondMax = numbers[i];
+			}
+			i++;
+		}
+
+		return secondMax;
+	}
+
+	public static boolean hasDifferentValues(int[] array) {
+		boolean allEqual = true;
+		int first = array[0];
+		for (int i = 1; i < array.length; i++) {
+			allEqual = allEqual && (first == array[i]);
+		}
+		return !allEqual;
+	}
 }
--- my-app/src/test/java/com/mycompany/app/SecondMaxTest.java
+++ my-app/src/test/java/com/mycompany/app/SecondMaxTest.java
@@ -1,97 +1,55 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
-
 import static com.mycompany.app.SecondMax.getSecondMax;
 
-public class SecondMaxTest 
-{
-    @Test
-    public void testEmptyArray(){
-    	boolean thrown = false;
-    	try{
-    		int[] emptyArr = {};
-    		getSecondMax(emptyArr);
-    	}
-    	catch(IllegalArgumentException exception){
-    		thrown = true;
-    	}
-    	assertTrue(thrown);
-    }
-    
-    @Test
-    public void testOneElementArray(){
-    	boolean thrown = false;
-    	try{
-    		int[] oneElementArr = {1};
-    		getSecondMax(oneElementArr);
-    	}
-    	catch(IllegalArgumentException exception){
-    		thrown = true;
-		}
-    	assertTrue(thrown);
-    }
-    
-    @Test
-    public void testEqualElementsArray(){
-	boolean thrown = false;
-	try{
-		int[] equalElementsArr = {1,1,1};
+public class SecondMaxTest {
+	@Test(expected = IllegalArgumentException.class)
+	public void testEmptyArray() {
+		int[] emptyArr = {};
+		getSecondMax(emptyArr);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testOneElementArray() {
+		int[] oneElementArr = { 1 };
+		getSecondMax(oneElementArr);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testEqualElementsArray() {
+		int[] equalElementsArr = { 1, 1, 1 };
 		getSecondMax(equalElementsArr);
 	}
-	catch(IllegalArgumentException exception){
-		thrown = true;
+
+	@Test
+	public void testSmallArray() {
+		int[] smallArr = { 1, 2 };
+		assertEquals(getSecondMax(smallArr), 1);
 	}
-	assertTrue(thrown);
-    }
-    
-    @Test
-    public void testSmallArray(){
-		int[] smallArr = {1,2};
-		assertEquals(getSecondMax(smallArr),1);
-    }
-    
-    @Test
-    public void testMiddleArray(){
-		int[] middleArr = {1,1,1,1,2,2,2,2,2};
-		assertEquals(getSecondMax(middleArr),1);
-    }
-    
-    @Test
-    public void testAnotherArray(){
-    	boolean thrown = false;
-    	try{
-    		int[] anotherArr = {1,1,1,1,1,1};
-    		getSecondMax(anotherArr);
-    	}
-    	catch(IllegalArgumentException exception){
-    		thrown = true;
-    	}
-    	assertTrue(thrown);
-    }
-    
-    @Test
-    public void testAnotherMiddleArray(){
-		int[] middleArr = {1,2,3,4,5,6,7,8,9,10};
-		assertEquals(getSecondMax(middleArr),9);
-    }
-    
-    @Test
-    public void testAndAnotherArray(){
-		int[] middleArr = {1,2,3,4,5,6,7,8,9,9,10};
-		assertEquals(getSecondMax(middleArr),9);
-    }
-    
-    @Test
-    public void testAndNextArray(){
-		int[] middleArr = {1,2,3,4,5,6,7,8,9,10,10};
-		assertEquals(getSecondMax(middleArr),9);
-    }
+
+	@Test
+	public void testTwoSecondMaxElementsArray() {
+		int[] testArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10 };
+		assertEquals(getSecondMax(testArr), 9);
+	}
+
+	@Test
+	public void testTwoMaxElementArray() {
+		int[] testArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10 };
+		assertEquals(getSecondMax(testArr), 9);
+	}
+
+	@Test
+	public void testMinIntegerContainedArray() {
+		int[] testArr = { Integer.MIN_VALUE, Integer.MIN_VALUE + 1 };
+		assertEquals(getSecondMax(testArr), Integer.MIN_VALUE);
+	}
+
+	@Test
+	public void testMaxIntegerContainedArray() {
+		int[] testArr = { Integer.MAX_VALUE - 1, Integer.MAX_VALUE };
+		assertEquals(getSecondMax(testArr), Integer.MAX_VALUE - 1);
+	}
 }
